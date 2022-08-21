57caee42b959a76e89d82551b540a53430074a1b
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-08-21 23:26:28 +0900

    Add files via upload
    # Programmer:  Patrick Junhee Kim, Andrew Gunhee Kim
    # Date: 2022/8/21
    # File name: ff
    # Using C + Language and Arduino to enable making a connection between two UWB sensors.

---
+++ ff.ino
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2015 by Thomas Trojer <thomas@trojer.net>
+ * Decawave DW1000 library for arduino.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @file BasicReceiver.ino
+ * Use this to test simple sender/receiver functionality with two
+ * DW1000. Complements the "BasicSender" example sketch.
+ * 
+ * @todo
+ *  - move strings to flash (less RAM consumption)
+ *  
+ */
+
+#include <SPI.h>
+#include <DW1000.h>
+
+// connection pins
+const uint8_t PIN_RST = 9; // reset pin
+const uint8_t PIN_IRQ = 2; // irq pin
+const uint8_t PIN_SS = SS; // spi select pin
+
+// DEBUG packet sent status and count
+volatile boolean received = false;
+volatile boolean error = false;
+volatile int16_t numReceived = 0; // todo check int type
+String message;
+
+void setup() {
+  // DEBUG monitoring
+  Serial.begin(9600);
+  // initialize the driver
+  DW1000.begin(PIN_IRQ, PIN_RST);
+  DW1000.select(PIN_SS);
+  // general configuration
+  DW1000.newConfiguration();
+  DW1000.setDefaults();
+  DW1000.setDeviceAddress(6);
+  DW1000.setNetworkId(10);
+  DW1000.enableMode(DW1000.MODE_LONGDATA_RANGE_LOWPOWER);
+  DW1000.commitConfiguration();
+  // DEBUG chip info and registers pretty printed
+  char msg[128];
+  // attach callback for (successfully) received messages
+  DW1000.attachReceivedHandler(handleReceived);
+  DW1000.attachReceiveFailedHandler(handleError);
+  DW1000.attachErrorHandler(handleError);
+  // start reception
+  receiver();
+}
+
+void handleReceived() {
+  // status change on reception success
+  received = true;
+}
+
+void handleError() {
+  error = true;
+}
+
+void receiver() {
+  DW1000.newReceive();
+  DW1000.setDefaults();
+  // so we don't need to restart the receiver manually
+  DW1000.receivePermanently(true);
+  DW1000.startReceive();
+}
+
+void loop() {
+  // enter on confirmation of ISR status change (successfully received)
+  if (received) {
+    numReceived++;
+    // get data as string
+    DW1000.getData(message);
+     Serial.print(DW1000.getReceiveQuality());
+     Serial.print(",");
+    Serial.println(message);
+    received = false;
+  }
+  if (error) {
+    Serial.println("Error receiving a message");
+    error = false;
+    DW1000.getData(message);
+    Serial.print("Error data is ... "); Serial.println(message);
+  }
+}
